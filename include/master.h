00d6807ad867970fc3e0bd44ad03a531985abbb3
vinay-004 <vinay.vjjain.jai4@gmail.com>
2020-11-25 03:45:02 -0500

    adding cyclic

--- include/master.h
+++ include/master.h
@@ -24,5 +24,4 @@ void masterMPI_Horizontal(ConfigData *data, float *pixels);
 void masterMPI_Vertical(ConfigData *data, float *pixels); 
 void masterMPI_Block(ConfigData *data, float *pixels);
 void masterMPI_CyclicVertical(ConfigData *data, float *pixels);
-void masterCyclesV(ConfigData *data, float *pixels);
 #endif
--- include/slave.h
+++ include/slave.h
@@ -8,5 +8,4 @@ void slaveMPIHorizontal(ConfigData *data);
 void slaveMPIVertical(ConfigData *data);
 void slaveMPIBlock(ConfigData *data);
 void slaveMPICylicVertical(ConfigData *data);
-void slaveCyclesV(ConfigData *data);
 #endif
