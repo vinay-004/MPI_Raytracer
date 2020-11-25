6a82daf608b6af2f6c142692ee7bdfce72c90e2e
vinay-004 <vinay.vjjain.jai4@gmail.com>
2020-11-24 23:51:41 -0500

    adding horizontal inscript

--- runner_mpi.sh
+++ runner_mpi.sh
@@ -57,7 +57,7 @@ module load openmpi
 # Sequential
 #srun -n $SLURM_NPROCS raytrace_mpi -h 100 -w 100 -c configs/twhitted.xml -p none 
 # Static Strips
-srun -n $SLURM_NPROCS raytrace_mpi -h 100 -w 100 -c configs/twhitted.xml -p static_strips_vertical
+srun -n $SLURM_NPROCS raytrace_mpi -h 100 -w 100 -c configs/twhitted.xml -p static_strips_horizontal
 # Static Cycles
 # srun -n $SLURM_NPROCS raytrace_mpi -h 100 -w 100 -c configs/twhitted.xml -p static_cycles_vertical -cs 1
 # Static Blocks
